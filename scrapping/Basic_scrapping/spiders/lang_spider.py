675dbec1861a599b612ad4ef80e6a53f24bcf31c
Théo Massard <massar_t@etna-alternance.net>
2016-04-29 14:23:31 +0200

    [up]lang_spider fixed

--- scrapping/Basic_scrapping/spiders/lang_spider.py
+++ scrapping/Basic_scrapping/spiders/lang_spider.py
@@ -5,11 +5,6 @@ import scrapy
 class basic_scrapping(scrapy.Spider):
     """Spider looking for languages-related content"""
     name = "basic_scrapping"
-=======
-class LangSpider(scrapy.Spider):
-    """Spider looking for languages-related content"""
-    name = "lang"
->>>>>>> d58632e2f39bfd83394d0b62750311989e49ba54
     allowed_domains = ["wikipedia.org"]
     start_urls = [
         "https://en.wikipedia.org/wiki/Functional_programming",
@@ -20,8 +15,4 @@ class LangSpider(scrapy.Spider):
     def parse(self, response):
         filename = response.url.split("/")[-2] + '.html'
         with open(filename, 'wb') as f:
-<<<<<<< HEAD
             f.write(response.body)
-=======
-            f.write(response.body)
->>>>>>> d58632e2f39bfd83394d0b62750311989e49ba54
