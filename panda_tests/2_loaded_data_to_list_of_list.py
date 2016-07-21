0760cd8f0a7276d9cdacd58f72c4bdfa1ec62139
Theo Massard <massar_t@etna-alternance.net>
2016-07-21 13:39:57 +0200

    added main() to 2_loaded_data_to_list_of_list

--- panda_tests/2_loaded_data_to_list_of_list.py
+++ panda_tests/2_loaded_data_to_list_of_list.py
@@ -1,7 +1,14 @@
 # coding: utf-8
-for hit in hits:
-    curr = []
-    curr.append(hit["_source"]["source"])
-    curr.append(hit["_source"]["pictures"])
-    data_to_save.append(curr)
-    
+
+def main():
+    data_to_save = []
+    for hit in hits:
+        curr = []
+        curr.append(hit["_source"]["source"])
+        curr.append(hit["_source"]["pictures"])
+        data_to_save.append(curr)
+    return data_to_save
+
+
+if __name__ == '__main__':
+    main()
