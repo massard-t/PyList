0aa305d1e0ae9f8afb116453c8745caaf096744b
Theo Massard <massar_t@etna-alternance.net>
2016-07-21 13:42:51 +0200

    functionnized 4_clea_na_values

--- panda_tests/4_clear_na_values.py
+++ panda_tests/4_clear_na_values.py
@@ -1,9 +1,9 @@
 # coding: utf-8
+import panda as pd
 
 
-existing_csv = pd.read_csv('test_append_csv.csv')
-existing_csv.dropna()
-existing_csv
-csv_without_na = existing_csv.dropna()
-csv_without_na
-csv_without_na.to_csv('clean_csv.csv')
+def clear_na_from_csv(csv_source, csv_dest):
+    existing_csv = pd.read_csv(csv_source)
+    csv_without_na = existing_csv.dropna()
+    csv_without_na.to_csv(csv_dest)
+
