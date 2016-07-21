53f957b9401c2360bb434847bf69850c57714ee7
Theo Massard <massar_t@etna-alternance.net>
2016-07-21 13:41:41 +0200

    functionnized 3_from_list_append_to_csv

--- panda_tests/3_from_list_append_to_csv.py
+++ panda_tests/3_from_list_append_to_csv.py
@@ -1,7 +1,12 @@
 # coding: utf-8
 import pandas as pd
-data_frame_from_list = pd.DataFrame(data_to_save)
-data_frame_from_list
-with open('test_append_csv.csv', 'a') as f:
-    data_frame_from_list.to_csv(f, header=False)
-    
+
+
+def write_to_csv(data_to_save, csv_file):
+    data_frame_from_list = pd.DataFrame(data_to_save)
+    try:
+        with open(csv_file, 'a') as f:
+            data_frame_from_list.to_csv(f, header=False)
+    except Exception:
+        return False
+    return True
