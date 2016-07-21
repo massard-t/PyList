1991131cc88143e75e987177463b56f2c0609417
Theo Massard <massar_t@etna-alternance.net>
2016-07-21 13:35:28 +0200

    added main to pjson

--- json_management/pjson.py
+++ json_management/pjson.py
@@ -3,8 +3,13 @@ import json
 import sys
 
 
-try:
-    input_str = sys.stdin.read()
-    print json.dumps(json.loads(input_str), sort_keys=True, indent=2)
-except ValueError, e:
-    print "error %snError: %s"%(input_str, str(e))
+def main():
+    try:
+        input_str = sys.stdin.read()
+        print(json.dumps(json.loads(input_str), sort_keys=True, indent=2))
+    except ValueError, e:
+        print "error %snError: %s"%(input_str, str(e))
+
+
+if __name__ == '__main__':
+        main()
